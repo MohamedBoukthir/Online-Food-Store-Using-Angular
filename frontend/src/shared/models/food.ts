866dbee7eb56f2eb95ddf1615f3b055b127c59ce
MohamedBoukthir <medboukthir7@gmail.com>
2023-02-12 03:54:45 +0100

    food project

---
+++ frontend/src/shared/models/food.ts
@@ -0,0 +1,12 @@
+export class Food{
+  id!:string;
+  name!:string;
+  price!:Number;
+  tags?:string[];
+  favorite!:Boolean;
+  stars!:Number;
+  imageUrl!:string;
+  origins!: string[];
+  cookTime!:string;
+
+}
