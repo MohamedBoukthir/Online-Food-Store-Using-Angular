ea93914accfab764d9e2431314dc67b382079fa7
MohamedBoukthir <medboukthir7@gmail.com>
2023-02-12 20:06:48 +0100

    Mean Stack

--- frontend/src/app/components/pages/home/home.component.ts
+++ frontend/src/app/components/pages/home/home.component.ts
@@ -15,6 +15,8 @@ export class HomeComponent implements OnInit {
       activatedRoute.params.subscribe((params) =>{
         if(params.searchTerm)
         this.foods = this.foodServices.getAllFoodsBySearchTerm(params.searchTerm);
+        else if(params.tag)
+        this.foods = this.foodServices.getAllFoodsByTag(params.tag);
         else
         this.foods = foodServices.getAll();
 
--- frontend/src/app/services/food.service.ts
+++ frontend/src/app/services/food.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { sample_foods } from 'src/data';
+import { sample_foods, sample_tags } from 'src/data';
 import { Food } from 'src/shared/models/food';
+import { Tag } from 'src/shared/models/tag';
 
 @Injectable({
   providedIn: 'root'
@@ -12,8 +13,18 @@ export class FoodService {
   getAll():Food[]{
     return sample_foods;
   }
-  
+
   getAllFoodsBySearchTerm(searchTerm:string){
     return this.getAll().filter(food => food.name.toLowerCase().includes(searchTerm.toLowerCase()))
+  }
+
+  getAllTags():Tag[]{
+    return sample_tags;
+  }
+
+  getAllFoodsByTag(tag: string): Food[] {
+    return tag === "All" ?
+      this.getAll() :
+      this.getAll().filter(food => food.tags?.includes(tag));
   }
 }
